ee925179d9eda8a021277b9d63e600ed3444d4d2
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-15 20:33:18 -0500

    primer commit sbastian aprte 2

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,18 +1,41 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class PostController {
 
+    @Autowired
+    PostRepository postRepository;
+
     @GetMapping(value = {"", "/"})
-    public String listPost(){
+    public String listPost(Model model, RedirectAttributes attr) {
+        List<Post> listapost = postRepository.findAll();
+        Date now = new Date();// extraer fecha de hoy
+        for (Post post : listapost) {
+            Date dateresult = new Date((now.getTime() - post.getCreationDate().getTime()));
+            post.setCreationDate(dateresult);
+        }
+        model.addAttribute("listapost", listapost);
         return "post/list";
     }
 
     @GetMapping("/post/new")
-    public String newPost(){
+    public String newPost() {
         return "post/new";
     }
 
