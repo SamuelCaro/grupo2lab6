f4b5000c92c5ebb808f1974024a27d664767f17b
Samuel Caro <a20170086@pucp.edu.pe>
2020-06-29 20:03:50 -0500

    Caro

--- src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
+++ src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
@@ -1,4 +1,37 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class ProductWebService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping(value = "/ws/post/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity listarPosts(@RequestParam("query") String query) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        responseMap.put("estado", "ok");
+        responseMap.put("listaPosts", postRepository.listarFechaDesc());
+        return new ResponseEntity(responseMap, HttpStatus.OK);
+
+
+    }
+
 }
+
--- src/main/java/sw2/lab6/teletok/repository/PostRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostRepository.java
@@ -1,9 +1,19 @@
 package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.PostComment;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Query(value = "SELECT p.description FROM post p ORDER BY creation_date DESC;",nativeQuery = true)
+    List<Post> listarFechaDesc();
+
+
+
 }
