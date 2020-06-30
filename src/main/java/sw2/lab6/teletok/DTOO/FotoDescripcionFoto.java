562ccd7552d5a5ff43a59e689797ea7759678ae7
Royer <r.yangali@pucp.pe>
2020-06-29 20:50:41 -0500

    con fe

---
+++ src/main/java/sw2/lab6/teletok/DTOO/FotoDescripcionFoto.java
@@ -0,0 +1,10 @@
+package sw2.lab6.teletok.DTOO;
+
+public interface FotoDescripcionFoto {
+    String getDescription();
+    String getMediaurl();
+    String getCode();
+
+
+
+}
--- src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
+++ src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
@@ -11,8 +11,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sw2.lab6.teletok.DTOO.FotoDescripcionFoto;
+import sw2.lab6.teletok.DTOO.TokenPostMsg;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.PostComment;
+import sw2.lab6.teletok.repository.PostCommentRepository;
 import sw2.lab6.teletok.repository.PostRepository;
+
 import java.util.HashMap;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.*;
 import sw2.lab6.teletok.DTOO.LoginDto;
 import sw2.lab6.teletok.entity.Token;
@@ -27,6 +35,8 @@ public class ProductWebService {
 
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    PostCommentRepository postCommentRepository;
 
     @GetMapping(value = "/ws/post/list", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity listarPosts(@RequestParam("query") String query) {
@@ -40,7 +50,7 @@ public class ProductWebService {
 
     }
 
-    UserRepository userRepository;
+
     @Autowired
     TokenRepository tokenRepository;
 
@@ -58,7 +68,7 @@ public class ProductWebService {
         }
         User usuario = userRepository.findByUsername(username);
         boolean match = true; //passwordEncoder.matches(usuario.getPassword(), password);
-        if(usuario.getUsername().equalsIgnoreCase(username) && match){
+        if (usuario.getUsername().equalsIgnoreCase(username) && match) {
             String tokenx = getAlphaNumericString(36);
             Token token = new Token();
             token.setCode(tokenx);
@@ -67,13 +77,11 @@ public class ProductWebService {
             responseMap.put("status", "AUTHENTICATED");
             responseMap.put("token", token.getCode());
             return new ResponseEntity(responseMap, HttpStatus.OK);
-        }else{
+        } else {
             responseMap.put("error", "AUTH_FAILED");
             return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
         }
     }
-
-
 
 
     public String getAlphaNumericString(int n) {
@@ -100,6 +108,66 @@ public class ProductWebService {
         }
 
         return sb.toString();
+    }
+
+    //GUARDAR UN POST
+    @PostMapping(value = "/ws/post/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity guardarPost(
+            @RequestBody FotoDescripcionFoto tokenDescripcionFoto
+    ) {
+        HashMap<String, Object> responseMap = new HashMap<>();
+        Post post = null;
+        Optional<Token> opt = tokenRepository.findbyCode(tokenDescripcionFoto.getCode());
+        if (opt.isPresent()) {
+            if (tokenDescripcionFoto.getDescription() != null && tokenDescripcionFoto.getMediaurl() != null) {
+                post.setDescription(tokenDescripcionFoto.getDescription());
+                post.setMediaUrl(tokenDescripcionFoto.getMediaurl());
+                postRepository.save(post);
+                responseMap.put("id", post.getId()); //si token es válido, se envía el id
+                responseMap.put("status", "POST_CREATED"); //la variable de tipo HashMap envía la info
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+
+            } else {
+                if (tokenDescripcionFoto.getMediaurl() == null && tokenDescripcionFoto.getDescription() != null) {
+                    responseMap.put("error", "EMPTY_FILE"); //la variable de tipo HashMap envía la info
+                } else {
+                    responseMap.put("error", "UPLOAD_ERROR"); //la variable de tipo HashMap envía la info
+                }
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+
+            }
+        } else {
+            //ERROR DE TOKEN
+            responseMap.put("error", "TOKEN_INVALID"); //la variable de tipo HashMap envía la info
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    //GUARDAR UN COMENTARIO
+    @PostMapping(value = "/ws/post/comment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity postComentario(
+            @RequestBody TokenPostMsg tokenPostMsg {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        PostComment postComment = null;
+        Optional<Token> opt = tokenRepository.findbyCode(tokenPostMsg.getToken());
+
+        if (opt.isPresent()) {
+            postComment.setMessage(tokenPostMsg.getMessage());
+            postCommentRepository.save(postComment);
+            responseMap.put("commentId", postComment.getId());
+
+            responseMap.put("status", "COMMENT_CREATED"); //la variable de tipo HashMap envía la info
+            return new ResponseEntity(responseMap, HttpStatus.CREATED);
+
+        } else {
+            responseMap.put("error", "TOKEN_INVALID"); //la variable de tipo HashMap envía la info
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+
+        }
+
+
     }
 }
 
