55585fb12fcdbd5d7c79c36e35df938664a42e52
Royer <r.yangali@pucp.pe>
2020-06-29 19:25:08 -0500

    entidades f

--- src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
+++ src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
@@ -1,4 +1,7 @@
 package sw2.lab6.teletok.controller;
 
 public class ProductWebService {
+
+
+
 }
---
+++ src/main/java/sw2/lab6/teletok/entity/Role.java
@@ -0,0 +1,31 @@
+package sw2.lab6.teletok.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
