d59166022cc6935fb6ac175f7766c0662d979eb0
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-29 19:25:59 -0500

    inicio

--- src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
+++ src/main/java/sw2/lab6/teletok/controller/ProductWebService.java
@@ -1,4 +1,29 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+@RestController
+@CrossOrigin
 public class ProductWebService {
+
+
+
+    @PostMapping(value = "/user/signIn", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity signin(
+            @RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+
+        if (username.isEmpty() || password.isEmpty()) {
+            responseMap.put("id", product.getId());
+        }
+        responseMap.put("estado", "creado");
+        return new ResponseEntity(responseMap, HttpStatus.CREATED);
+
+    }
 }
